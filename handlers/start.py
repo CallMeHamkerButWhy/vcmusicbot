83a7408c9f5f6781c68da897836425a5ff65e43e
Adi <80750099+MeIsNoob05@users.noreply.github.com>
2021-05-20 15:00:54 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,12 +9,12 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @W2HSupport.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @ADDICT_ADITYA.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Support⚡️", url="https://t.me/W2HSupport"
+                        "Support⚡️", url="https://t.me/ADDICT_ADITYA"
                     )
                 ],
                 [
