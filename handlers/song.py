26ea0025b896b6feb6ca6a94e12cd3aa29a7370b
The-fsociety07 <84204876+The-fsociety07@users.noreply.github.com>
2021-06-05 12:51:08 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -61,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by ✯@Addict_Aditya✯**'
+        rep = '**🎵 Uploaded by ✯@The_fsociety✯**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -260,7 +260,7 @@ async def jssong(_, message):
     is_downloading = True
     text = message.text.split(None, 1)[1]
     query = text.replace(" ", "%20")
-    m = await message.reply_text("Searching... Via ✯W2HMusic✯")
+    m = await message.reply_text("Searching... Via ✯Fsociety✯")
     try:
         songs = await arq.saavn(query)
         sname = songs[0].song
