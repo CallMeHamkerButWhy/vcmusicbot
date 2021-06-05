3c6150129c8f2483f9173cc65cc949d7282b04fe
The-fsociety07 <84204876+The-fsociety07@users.noreply.github.com>
2021-06-05 13:21:11 +0530

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -20,7 +20,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "SONGSxOP_bot"
+        user.first_name =  "@axf_music_bot"
 
     try:
         await USER.join_chat(invitelink)
@@ -34,7 +34,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @SONGSxOP_bot to your Group and try again</b>",
+            "\n\nOr manually add @axf_music_bot to your Group and try again</b>",
         )
         return
     await message.reply_text(
